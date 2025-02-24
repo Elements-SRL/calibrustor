568611bad6319c87d5b1f767783cbb2f8298f116
Luca Rossi <lucarossi147@gmail.com>
2025-02-24 12:54:22 +0100

    feat: added calibration of vc i offset for device with input switches

--- src/calibration/strategies/vc_i_offset.rs
+++ src/calibration/strategies/vc_i_offset.rs
@@ -9,6 +9,13 @@ use crate::{
 };
 use std::collections::HashMap;
 
+fn calibrate<D: Device<Volt, Ampere>>(d: &D, cc: CalibContext<Volt, Ampere>) -> CalibrationResult<Volt, Ampere> {
+    let a = d.acquire(1.0);
+    let offsets = a.get_readout_means();
+    let cv = CalibrationValues::new_float(HashMap::from_iter(offsets.into_iter().enumerate()));
+    CalibrationResult::new(cc, cv)
+}
+
 #[derive(Default, Clone, Copy)]
 pub struct IOffsetStd;
 
@@ -17,9 +24,17 @@ where
     D: Device<Volt, Ampere>,
 {
     fn calibrate(&self, d: &D, cc: CalibContext<Volt, Ampere>) -> CalibrationResult<Volt, Ampere> {
-        let a = d.acquire(1.0);
-        let offsets = a.get_readout_means();
-        let cv = CalibrationValues::new_float(HashMap::from_iter(offsets.into_iter().enumerate()));
-        CalibrationResult::new(cc, cv)
+        calibrate(d, cc)
+    }
+}
+
+#[derive(Default, Clone, Copy)]
+pub struct IOffsetNoHands;
+impl<D> CalibrationStrategy<Volt, Ampere, D> for IOffsetNoHands
+where
+    D: Device<Volt, Ampere>,
+{
+    fn calibrate(&self, d: &D, cc: CalibContext<Volt, Ampere>) -> CalibrationResult<Volt, Ampere> {
+        calibrate(d, cc)
     }
 }
